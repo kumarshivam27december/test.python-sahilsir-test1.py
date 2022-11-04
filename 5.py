fec693c1824800e7605ea71f146bdd9059eab1c9
shivamkumar <kumarshivam16062004@gmail.com>
2022-11-05 01:38:04 +0530

    version 3.0(correction of problems)

--- 5.py
+++ 5.py
@@ -2,5 +2,46 @@
 #  no greater than 150  skip
 # no greater than 500,stop the loop
 
-a=int(input("enter your number"))
+# a=int(input("enter your number: "))
+# num = range(0,500)
 
+# if a%=5:
+#     print()
+
+
+# if a>150:
+#     pass
+# else a<500:
+  
+# for x in range(0,500):
+#     if x<150:break
+#     print(x)
+# else:
+#     print("stop the loop")
+x=int(input("enter your number: "))
+while x%5==0:
+    x+=1
+    if x>500:
+        continue
+    print(x)
+else:
+    print("the loop is over")
+        
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
--- 7.py
+++ 7.py
@@ -1,15 +1,64 @@
-'''
-5 4 3 2 1 
-4 3 2 1 
-3 2 1
-2 1
-1
-'''
+# # '''
+# # 5 4 3 2 1 
+# # 4 3 2 1 
+# # 3 2 1
+# # 2 1
+# # 1
+# # '''
 
-# for i in 
-n=5
-for j in range(1,n+1):
-    for i in range(1,j+1):
-        print(i,end=" ")
+# # # for i in 
+# # # n=5
+# # # for i in range(n+1,1):
+# # #     for j in range(n+1,i):
+# # #         print(i,end=" ")
+# # #     print()
+
+# # n=int(input())
+
+# # for i in range(n,0,-1):
+
+# #     j=i
+
+# # while j>0:
+
+# #     print(j,end='')
+
+# # j=j-1
+
+# # print()
+
+# # # If you want a output like :
+
+# # # ‘’’
+
+# # # 543214321321211
+
+# # # ‘’’
+
+# # # Then the code for the above output is :
+
+# # n=int(input())
+
+# # for i in range(n,0,-1):
+
+# #     j=i
+
+# # while j>0:
+# #     print(j,end='')
+
+# # j=j-1
+
+
+# rows = 5
+# # reverse loop
+# for i in range(rows, 0, -1):
+#     num = i
+#     for j in range(0, i):
+#         print(num, end=' ')
+#     print("\r")
+rows = 5
+for i in range(0, rows + 1):
+    for j in range(rows - i, 0, -1):
+        print(j, end=' ')
     print()
 
